35fea87a288566526999e6443402e45c5826044e
Hemachandrac22 <143271888+Hemachandrac22@users.noreply.github.com>
2025-03-12 10:42:06 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  cout << "This is the new line edited \n";
-  cout << "Hemachandra \n";
+  cout << "Hello from pes1ug22am044's Jenkins pipeline" << endl;
   return 0;
 }
